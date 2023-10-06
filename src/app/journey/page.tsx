ea2a748d6c11dbf241988f57e2317a4bb72d117d
Giovanni Vicentin <giovannifvicentin@gmail.com>
2023-10-06 00:12:11 -0300

    Create the journey page

--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -1,7 +1,62 @@
 const JourneyPage = () => {
   return (
-    <div>
-      <h1>Journey Page</h1>
+    <div className="flex flex-col items-center p-4 md:px-24">
+      <div className="max-w-5xl w-full text-center py-6">
+        <h1 className="text-3xl mb-6">My Journey</h1>
+        <h2 className="text-xl mb-6 text-justify leading-relaxed">
+          Hi there! I'm{' '}
+          <span className="dark:text-violet-400 text-violet-700 border-b border-muted">
+            Giovanni Vicentin
+          </span>
+          , but you can call me Gi. I'm a Software Engineer based in Brazil,
+          currently working as a Full Stack Developer at Talst, a startup.
+        </h2>
+        <p className="mb-6 text-justify leading-relaxed">
+          Ever since my childhood, I've been drawn to creating and solving
+          problems. This is why mathematics has always been my favorite subject
+          in school—it consistently presented challenges that pushed me to find
+          solutions. In high school, I found myself torn between mathematics and
+          philosophy, both of which made me deeply reflect on the everyday
+          issues we face.
+        </p>
+        <p className="mb-6 text-justify leading-relaxed">
+          I grew up playing on computers, and I'll admit, it turned me into a
+          computer enthusiast. I was the go-to person for fixing computer issues
+          for my friends and family, often consulting forums or videos—just like
+          many developers do. This experience nurtured my autodidactic nature
+          and deepened my love for technology.
+        </p>
+        <p className="mb-6 text-justify leading-relaxed">
+          I began my journey with programming in 2020 when I was 16 years old.
+          However, my battle with depression took a toll, causing me to hit a
+          standstill. I picked up my studies again in 2021 and I've been loving
+          it ever since. This experience has shaped me into a resilient
+          individual who continually perseveres, never ceasing to learn and grow
+          in the realm of programming.
+        </p>
+        <p className="mb-6 text-justify leading-relaxed">
+          I began my career as a software engineer in 2022, specializing in
+          backend development using Python, MySQL, and VBA. During this time, I
+          worked on creating Robotic Process Automation (RPA) solutions and
+          streamlined processes for automation. This experience solidified my
+          understanding of algorithms, logic, UML, databases, and the dynamics
+          of a technology team. After six months, I transitioned to using NodeJS
+          as part of my tech stack, integrating technologies such as TypeScript,
+          NestJS, and TypeORM, while continuing with MySQL. This period was
+          transformative as I honed my skills in CRUD operations, API
+          development, and project architecture and realized the significance of
+          documentation and the impact of agile methodologies on productivity.
+          Another six months later, I delved into frontend development, working
+          with React, Next.js, and Tailwind. This phase enriched my perspective
+          on the client side of applications, and interface design, and
+          underscored the notion that FrontEnd development is as challenging and
+          rewarding as backend. It's a journey I genuinely relish.
+        </p>
+        <p className="mb-6 text-justify leading-relaxed">
+          I'm currently working as a Full Stack Developer, utilizing all these
+          technologies, depending on the project.
+        </p>
+      </div>
     </div>
   );
 };
