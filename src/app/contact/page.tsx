81b8532105e4f5c8774a8bc0257541a1e7ce2a87
Giovanni Vicentin <giovannifvicentin@gmail.com>
2023-10-08 16:45:48 -0300

    Align items in the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,13 +6,31 @@ const ContactPage = () => {
     <div className="flex flex-col p-4 md:px-24">
       <div className="max-w-4xl w-full py-6">
         <h1 className="text-3xl mb-6 text-center">Contact</h1>
-        <div className="">
-          <Label htmlFor="picture">E-mail</Label>
-          <Input type="email" placeholder="Email" className="max-w-md" />
-          <Label htmlFor="picture">Title</Label>
-          <Input type="text" placeholder="Title" className="max-w-md" />
-          <Label htmlFor="picture">Message</Label>
-          <Input type="text" placeholder="Message" className="max-w-md" />
+        <div className="flex flex-col items-center space-y-2">
+          <div className="flex flex-col items-start w-full max-w-md">
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="giovanni.vicentin@email.com"
+            />
+          </div>
+          <div className="flex flex-col items-start w-full max-w-md">
+            <Label htmlFor="subject">Subject</Label>
+            <Input
+              id="subject"
+              type="text"
+              placeholder="Write your subject here"
+            />
+          </div>
+          <div className="flex flex-col items-start w-full max-w-md">
+            <Label htmlFor="message">Message</Label>
+            <Input
+              id="message"
+              type="text"
+              placeholder="Write your message here"
+            />
+          </div>
         </div>
       </div>
     </div>
