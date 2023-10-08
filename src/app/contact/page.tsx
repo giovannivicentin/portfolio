737c7bce8043160f510e14e071df8d27860db91a
Giovanni Vicentin <giovannifvicentin@gmail.com>
2023-10-08 03:07:06 -0300

    Create the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,20 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '../../components/ui/input';
+
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="flex flex-col p-4 md:px-24">
+      <div className="max-w-4xl w-full py-6">
+        <h1 className="text-3xl mb-6 text-center">Contact</h1>
+        <div className="">
+          <Label htmlFor="picture">E-mail</Label>
+          <Input type="email" placeholder="Email" className="max-w-md" />
+          <Label htmlFor="picture">Title</Label>
+          <Input type="text" placeholder="Title" className="max-w-md" />
+          <Label htmlFor="picture">Message</Label>
+          <Input type="text" placeholder="Message" className="max-w-md" />
+        </div>
+      </div>
     </div>
   );
 };
