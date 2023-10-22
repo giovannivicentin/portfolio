99ad5a3accfc46c517d29a6e6a839e29b4eeadf5
Giovanni Vicentin <giovannifvicentin@gmail.com>
2023-10-22 17:48:39 -0300

    Update the navbar to be responsive for the small smartphones

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,40 +9,49 @@ function Navbar() {
       <Link href="/">
         <Button
           variant="ghost"
-          className="text-sm sm:text-xs w-24 md:w-28 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
+          className="text-sm sm:text-xs md:text-md w-24 md:w-28 sm:w-20 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
         >
           Home
         </Button>
       </Link>
 
-      <Separator orientation="vertical" className="h-4 md:h-6 mx-1 md:mx-2" />
+      <Separator
+        orientation="vertical"
+        className="h-4 md:h-6 sm:h-2 mx-1 md:mx-2 sm:mx-1"
+      />
 
       <Link href="/journey">
         <Button
           variant="ghost"
-          className="text-sm sm:text-xs w-24 md:w-28 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
+          className="text-sm sm:text-xs md:text-md w-24 md:w-28 sm:w-20 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
         >
           Journey
         </Button>
       </Link>
 
-      <Separator orientation="vertical" className="h-4 md:h-6 mx-1 md:mx-2" />
+      <Separator
+        orientation="vertical"
+        className="h-4 md:h-6 sm:h-2 mx-1 md:mx-2 sm:mx-1"
+      />
 
       <Link href="/projects">
         <Button
           variant="ghost"
-          className="text-sm sm:text-xs w-24 md:w-28 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
+          className="text-sm sm:text-xs w-24 md:w-28 sm:w-20 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
         >
           Projects
         </Button>
       </Link>
 
-      <Separator orientation="vertical" className="h-4 md:h-6 mx-1 md:mx-2" />
+      <Separator
+        orientation="vertical"
+        className="h-4 md:h-6 sm:h-2 mx-1 md:mx-2 sm:mx-1"
+      />
 
       <Link href="/contact">
         <Button
           variant="ghost"
-          className="text-sm sm:text-xs w-24 md:w-28 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
+          className="text-sm sm:text-xs md:text-md w-24 md:w-28 sm:w-20 hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
         >
           Contact
         </Button>
