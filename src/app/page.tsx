4b01f216635644f5dc18f7f8fd2b24bae1835de1
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-10-23 14:58:05 -0300

    Update the app page to minimize the loading screen time.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-'use client';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { LinkedInLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
 import TechStack from '@/components/tech-stack';
-import LoadingScreen from '@/components/loading-screen';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 0);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) return <LoadingScreen />;
-
   return (
     <div className="flex flex-col justify-center items-center mt-20 opacity-0 transform translate-y-10 animate-fadeInSlideUp">
       <div className="flex flex-col-reverse sm:flex-row items-start">
@@ -39,12 +24,11 @@ export default function Home() {
             finding wisdom and inspiration in their pages.
           </p>
           <div className="flex mt-5">
-            <Link href="/journey">
+            <Link href="/journey" prefetch>
               <Button className="text-md transition-transform transform hover:scale-105 hover:dark:bg-violet-400 hover:bg-violet-700">
                 Learn more about me
               </Button>
             </Link>
-
             <Link href="/Giovanni_s_Resume.pdf" download>
               <Button className="text-md ml-1.5 transition-transform transform hover:scale-105 hover:dark:bg-violet-400 hover:bg-violet-700">
                 Download CV
@@ -54,24 +38,21 @@ export default function Home() {
           <p className="mt-5 text-muted-foreground text-lg">Find me on:</p>
           <div className="flex mt-5">
             <Link href="https://www.linkedin.com/in/giovannivicentin/">
-              {' '}
               <LinkedInLogoIcon className="w-7 h-7 transition-transform transform hover:scale-110 hover:dark:text-violet-400 hover:text-violet-700" />
             </Link>
-
             <Link href="https://github.com/giovannivicentin">
-              {' '}
               <GitHubLogoIcon className="w-7 h-7 ml-4 transition-transform transform hover:scale-110 hover:dark:text-violet-400 hover:text-violet-700" />
             </Link>
           </div>
           <TechStack />
         </div>
-
         <Image
           src="/profile.png"
           width={150}
           height={150}
-          alt="AI Profile Image"
+          alt="Profile Image"
           className="rounded-full mx-auto mb-10 sm:mx-0 sm:mb-0"
+          priority
         ></Image>
       </div>
     </div>
