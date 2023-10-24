468f5a42b7417874de2b1c003dc8ea91c9fd3fac
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-10-24 10:57:55 -0300

    Update the metadata in layout from app page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const poppins = Poppins({
   weight: '300',
@@ -19,8 +20,11 @@ const sintony = Sintony({
 });
 
 export const metadata: Metadata = {
-  title: "Giovanni Vicentin's Portfolio",
-  description: 'Giovanni Vicentin personal website',
+  title: 'Giovanni Vicentin - Professional Portfolio',
+  description:
+    'Explore the professional portfolio of Giovanni Vicentin. Discover his expertise, projects, and professional journey in the industry.',
+  authors: [{ name: 'Giovanni Vicentin' }],
+  metadataBase: new URL('https://giovannivicentn.com'),
 };
 
 export default function RootLayout({
@@ -33,6 +37,37 @@ export default function RootLayout({
       lang="en"
       className={`flex min-h-screen flex-col items-center justify-between bg-background text-foreground md:px-24 ${poppins.className}`}
     >
+      <Head>
+        <meta name="author" content="Giovanni Vicentin" />
+        <meta
+          name="description"
+          content="Explore the professional portfolio of Giovanni Vicentin. Discover his expertise, projects, and professional journey in the industry."
+        />
+        <meta
+          name="keywords"
+          content="giovanni vicentin, portfolio, portifolio, giovanni vicentin portifolio, giovanni vicentn portfolio, Giovanni Vicentin"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content="Giovanni Vicentin - Professional Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Explore the professional portfolio of Giovanni Vicentin. Discover his expertise, projects, and professional journey in the industry."
+        />
+        <meta property="og:image" content="public\logo.png" />
+        <meta property="og:url" content="https://www.giovannivicentin.com" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Giovanni Vicentin's Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Giovanni Vicentin - Professional Portfolio"
+        />
+        <meta name="twitter:image" content="public\logo.png" />
+        <meta name="twitter:url" content="https://www.giovannivicentin.com" />
+      </Head>
       <body className="flex flex-col flex-1 w-full items-center">
         <ThemeProvider
           attribute="class"
