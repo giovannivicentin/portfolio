8c6a997a64b0f7748837e050cb82057980c75deb
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-11-03 17:38:21 -0300

    Refactor theme selector: Replace dropdown with toggle button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,41 +3,37 @@
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import { Toggle } from '@/components/ui/toggle';
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, resolvedTheme, setTheme } = useTheme();
+
+  const handleToggle = () => {
+    if (resolvedTheme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
+  const displayIcon = () => {
+    if (theme === 'system') {
+      return resolvedTheme === 'dark' ? (
+        <Sun className="h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 dark:hover:text-violet-400 hover:text-violet-700" />
+      ) : (
+        <Moon className="h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 dark:hover:text-violet-400 hover:text-violet-700" />
+      );
+    }
+    return theme === 'dark' ? (
+      <Sun className="h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 dark:hover:text-violet-400 hover:text-violet-700" />
+    ) : (
+      <Moon className="h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 dark:hover:text-violet-400 hover:text-violet-700" />
+    );
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="hover:dark:text-violet-400 hover:text-violet-700 hover:bg-transparent"
-        >
-          <Sun className="h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-3.5 w-3.5 sm:w-5 sm:h-5 xl:h-6 xl:w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Toggle variant="default" aria-label="Toggle theme" onClick={handleToggle}>
+      {displayIcon()}
+    </Toggle>
   );
 }
